83d439d66bd9d5a29bdc73b9953e58611455df85
Jeff Gerber <jallengerber@gmail.com>
2015-06-18 19:15:50 -0700

    initial commit

--- app/script.js
+++ app/script.js
@@ -1,4 +1,4 @@
-
+'use strict';
 
 $(function() {
 
@@ -18,8 +18,28 @@ $(function() {
       var noun = response.word;
       $("#noun").text(noun);
     });
+  });
+
+  $('#submitWords').on('submit', function(e) {
+    e.preventDefault();
+
+    var adjective = $("input[name=adjective]").val();
+    var adjPost;
+
+    if (adjective) {
+      adjPost = {word: adjective};
+      $.post("adjective", adjPost, function(response) {
+        var adjectiveRes = response.msg;
+        $("#adjectiveRes").text(adjectiveRes);
+      });
+    }
+
+
 
   });
+
+
+
 
 });
 
